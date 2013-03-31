3ae835e69ee65e30b22a241c0335e785bcb6e8f2
Joonas Pääkkö <joonaspaakko@host-002.elisa>
2013-04-01 01:12:59 +0300

    Very extensive overhaul for the script
    From now on the script won't be run from the "Scripts events manager"
    via "Save document Event".
    
    The included action is needed for the plugin to function as extected.

--- Auto Save PSD.jsx
+++ Auto Save PSD.jsx
@@ -1,33 +1,52 @@
 // (c) Copyright 2005.  Adobe Systems, Incorporated.  All rights reserved.
 
+// Auto Save PSD.jsx 1.3.
+// This file was modified out of "Save Extra JPEG.jsx".
+// Modifications by Joonas Pääkkö
+// https://github.com/joonaspaakko
+
 /*
-@@@BUILDINFO@@@ Auto Save PSD.jsx 1.2
+
+    <javascriptresource>
+    <name>$$$/JavaScripts/AutoSavePSD/Menu=Auto Save PSD...</name>
+    <category>Auto Save</category>
+    <enableinfo>true</enableinfo>
+    <eventid>64feff0a-8271-436f-8c59-d2105497d902</eventid>
+    </javascriptresource>
+
 */
 
-var begDesc = "$$$/JavaScripts/AutoSavePSD/Description=This script is designed to be used as a script that runs after a save event. The script will save a copy of the current PSD document in a new folder ( current_filename_autoSave ) next to the current active document." // endDesc
-var begName = "$$$/JavaScripts/AutoSavePSD/MenuName=Auto Save PSD" // endName
+// enable double clicking from the Macintosh Finder or the Windows Explorer
+#target photoshop
 
 // on localized builds we pull the $$$/Strings from a .dat file, see documentation for more details
 $.localize = true;
 
 try {
 
-    if ( UsingAsACopy( arguments[0] ) ) {
-        alert(  localize( '$$$/JavaScripts/AutoSavePSDWarning=Save used As A Copy, extra file may not save correctly.' ) );
+    // Get the currently active document.
+    var doc = app.activeDocument;
+
+    // If document has been saved once...
+    if ( doc.saved ) {
+        // One document save is needed so that we know where to save the psd.
+        doc.save();
+    }
+    // Document has not been saved yet...
+    else {
+
+        // An action is triggered to prompt save as dialog.
+        // You'd think that this would be easy to do, but I
+        // couldn't figure out a better way for doing this.
+        app.doAction('save','Auto Save PSD');
+
     }
 
-    if ( IsBeginSaveEvent( arguments[0] ) ) {
-        alert(  localize( '$$$/JavaScripts/AutoSavePSDError=Auto Save PSD should only be used with the Save Document event and not the Start Save Document event.^rAuto Save PSD aborted!' ) );
-        throw( "DONE" );
-    }
-
-    var doc = activeDocument;
+    // Save additional psd file...
     var data = GetDataFromDocument( doc );
+    AutoSavePSD( doc, data );
 
-    if ( doc.activeLayer.allLocked && data.extension.toLowerCase() == 'psd' ) {
-        AutoSavePSD( data );
-        doc.activeLayer.allLocked = false;
-    }
+
 
 } // try end
 
@@ -52,7 +71,8 @@ catch( e ) {
 // Params: folder, filename, extension
 // Output: file saved as a copy next to the current active document
 ///////////////////////////////////////////////////////////////////////////////
-function AutoSavePSD( data ) {
+function AutoSavePSD( doc, data ) {
+
 
         // Save as .psd
         var psd_Opt = new PhotoshopSaveOptions();
@@ -61,82 +81,31 @@ function AutoSavePSD( data ) {
         psd_Opt.annotations = true; // Preserve annonations.
         psd_Opt.alphaChannels = true; // Preserve alpha channels.
 
-        var psd_Extension = '.psd';
-        if ( "" == data.extension ) {
-            psd_Extension = "";
-        }
-
-        // Excuse me sir. What time is it? ...where am I?
-        var time = new Date();
-        var seconds = time.getSeconds();
-        var sec = seconds < 10 ? '0' + seconds : seconds;
-        var minutes = time.getMinutes();
-        var min = minutes < 10 ? '0' + minutes : minutes;
-        var hours = time.getHours();
-        var hour = hours < 10 ? '0' + hours : hours;
-        var day = time.getDate();
-        var dd = day < 10 ? '0' + day : day;
-        var month = time.getMonth() + 1;
-        var mm = month < 10 ? '0' + month : month;
-        var yyyy = time.getFullYear();
-
-        date =  yyyy + '-' + mm + '-' + dd + '-' + hour + '-' + min + '-' + sec;
+         // Excuse me sir. What time is it? ...where am I?
+        var time                      = new Date();
+        var seconds                   = time.getSeconds();
+        var sec                       = seconds < 10 ? '0' + seconds : seconds;
+        var minutes                   = time.getMinutes();
+        var min                       = minutes < 10 ? '0' + minutes : minutes;
+        var hours                     = time.getHours();
+        var hour                      = hours < 10 ? '0' + hours : hours;
+        var day                       = time.getDate();
+        var dd                        = day < 10 ? '0' + day : day;
+        var month                     = time.getMonth() + 1;
+        var mm                        = month < 10 ? '0' + month : month;
+        var yyyy                      = time.getFullYear();
+        var date                      =  yyyy + '-' + mm + '-' + dd + '-' + hour + '-' + min + '-' + sec;
 
         // New folder for the auto saves...
-        var asFolder = new Folder( data.folder + '/' + data.fileName + '_autoSave/');
+        var fileName                  = data.fileName;
+        var folderPath                = data.folder + '/' + fileName + ' (-autoSave-)/';
+        var asFolder                  = new Folder( folderPath );
+
+        // If folder doesn't exist, let's create one.
         if( !asFolder.exists ) asFolder.create();
 
-        // Save document as....
-        activeDocument.saveAs( File( data.folder + '/' + data.fileName + '_autoSave/' + data.fileName + ' ' + date + psd_Extension ), psd_Opt, true );
-
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// Function: UsingAsACopy
-// Use: find out if the user used 'As A Copy'
-// Input: action descriptor from the event that just occured
-// Output: boolean that 'As A Copy' was checked
-// Note: On script events the script gets passed in the actual action that
-// occured we can look inside the action descriptor and pull information out
-// in this case we are looking for the keyCopy
-///////////////////////////////////////////////////////////////////////////////
-function UsingAsACopy( actionDescriptor ) {
-    var usingKeyCopy = false;
-    if ( undefined != actionDescriptor ) {
-        if ( "ActionDescriptor" == actionDescriptor.typename ) {
-            var keyCopy = charIDToTypeID( "Cpy " );
-            if ( actionDescriptor.hasKey( keyCopy ) ) {
-                usingKeyCopy = actionDescriptor.getBoolean( keyCopy );
-            }
-        }
-    }
-    return usingKeyCopy;
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// Function: IsBeginSaveEvent
-// Use: find out if the user used 'Start Save Document' event
-// Input: action descriptor from the event that just occured
-// Output: boolean that this is the 'Start Save Event' is occuring
-// Note: On script events the script gets passed in the actual action that
-// occured we can look inside the action descriptor and pull information out
-// in this case we are looking for the "saveStage" to not be "saveBegin"
-///////////////////////////////////////////////////////////////////////////////
-function IsBeginSaveEvent( actionDescriptor ) {
-    var usingStartSave = false;
-    if ( undefined != actionDescriptor ) {
-        if ( "ActionDescriptor" == actionDescriptor.typename ) {
-            var keySaveStage = stringIDToTypeID( "saveStage" );
-            if ( actionDescriptor.hasKey( keySaveStage ) ) {
-                var typeSaveStage = actionDescriptor.getEnumerationType( keySaveStage );
-                 var typeSaveStageType = stringIDToTypeID( "saveStageType" );
-                 var enumSaveStage = actionDescriptor.getEnumerationValue( keySaveStage );
-                 var enumSaveStageBegin = stringIDToTypeID( "saveBegin" );
-                 usingStartSave = enumSaveStage == enumSaveStageBegin && typeSaveStage == typeSaveStageType;
-            }
-        }
-    }
-    return usingStartSave;
+        // Creates the additional .psd file.
+        doc.saveAs( File( folderPath + fileName + ' ' + date + '.psd' ), psd_Opt, true );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -146,14 +115,18 @@ function IsBeginSaveEvent( actionDescriptor ) {
 // Output: Object containing folder, fileName, fileType, extension
 ///////////////////////////////////////////////////////////////////////////////
 function GetDataFromDocument( inDocument ) {
-    var data = new Object();
-    var fullPathStr = inDocument.fullName.toString();
-    var lastDot = fullPathStr.lastIndexOf( "." );
+
+    var data           = new Object();
+    var fullPathStr    = inDocument.fullName.toString();
+    var lastDot        = fullPathStr.lastIndexOf( "." );
     var fileNameNoPath = fullPathStr.substr( 0, lastDot );
-    data.extension = fullPathStr.substr( lastDot + 1, fullPathStr.length );
-    var lastSlash = fullPathStr.lastIndexOf( "/" );
-    data.fileName = fileNameNoPath.substr( lastSlash + 1, fileNameNoPath.length );
-    data.folder = fileNameNoPath.substr( 0, lastSlash );
-    data.fileType = inDocument.fullName.type;
+    var lastSlash      = fullPathStr.lastIndexOf( "/" );
+
+    data.extension     = fullPathStr.substr( lastDot + 1, fullPathStr.length );
+    data.fileName      = fileNameNoPath.substr( lastSlash + 1, fileNameNoPath.length );
+    data.folder        = fileNameNoPath.substr( 0, lastSlash );
+    data.fileType      = inDocument.fullName.type;
+
     return data;
+
 }
