ab3f541eb230006720ec3b69020bf90731730570
Joonas Pääkkö <paakkoj@gmail.com>
2018-11-24 00:06:56 +0200

    Added: #3 & #4

--- Photoshop File Versions.jsx
+++ Photoshop File Versions.jsx
@@ -1,13 +1,18 @@
-// Auto Save PSD.jsx 1.5.
+// Auto Save PSD.jsx 1.6.
 // Author: Joonas Pääkkö
 // https://github.com/joonaspaakko/Photoshop-file-versions-script
 
 // Changelog:
 // ==========
 
+// 1.6.
+// - Change: versions folder name by including .psd extension. Sorting is a bit better like this.
+// - Change: I deemed that because of the description, it's better if version numbers are in the front. That way random numbers are not going to give you trouble.
+// - Added: a variable "useDocName" so you can choose to go with just the description or just the number.
+
 // 1.5.
 // - Removed: No need for the PS Action anymore.
-// - Fix: File naming now works by finding the largest number. Meaning you can now rename or delete files in the ()
+// - Fix: File naming now works by finding the largest number. Meaning you can now rename or delete files in the versions folder without weird jumps in numbers.
 // - Added: Optional alert that shows the name of the saved file. With this you can make a mental note of which version was which. Set variable 'notify' = false if you dont' want that.
 // - Added: Optional prompt for adding a small description in the filename. Can be disabled by setting the variable "shortDescription" = false;
 // - Change: Renamed the save folder.
@@ -18,9 +23,10 @@
 
 #target photoshop
 
-var notify = true; // Boolean.
-var shortDescription = true; // Boolean. A prompt where use can describe changes briefly.
-var versionsFolderName = ' (Versions)'; // String.
+var notify             = true; // Boolean. Shows the newly saved verions full filename after saving.
+var shortDescription   = true; // Boolean. A prompt where use can describe changes briefly. Leaving the description empty does the same thing as canceling.
+var useDocName         = true; // Boolean. You can use this to get more room for the description.
+var versionsFolderName = ' (Versions)'; // String. This is a suffix for the versions folder: "My Document.psd (Versions)".
 
 try {
 
@@ -70,20 +76,23 @@ function saveNewVersion( doc, docName ) {
   if( !outputFolder.exists ) { outputFolder.create(); }
 	
   // Get list of all the current auto saved files
-  var verisonsList = outputFolder.getFiles( docName + '*' + psd );
-	var suffix = getVersionNumber( verisonsList );
+  var verisonsList = outputFolder.getFiles( '*' + psd );
+	var versionsLength1 = verisonsList.length;
+	var number = getVersionNumber( docName, verisonsList );
 	
+	var descPrompt = null;
 	if ( shortDescription ) {
-		var descPrompt = prompt("Short description:", "");
+		descPrompt = prompt("Short description:", "");
 		if ( descPrompt ) {
-			shortDescription = '('+ descPrompt + ') ';
+			var descSpace = useDocName ? ' ' : '';
+			var descParenthesis = [ useDocName ? '(' : '', useDocName ? ')' : '' ];
+			shortDescription = descSpace + descParenthesis[0] + descPrompt + descParenthesis[1];
 		}
 		else {
 			shortDescription = '';
 		}
 	}
-
-  // Options for the soon to be Auto Saved PSD file
+	
   var psd_Opt               = new PhotoshopSaveOptions();
   psd_Opt.layers            = true; // Preserve layers.
   psd_Opt.embedColorProfile = true; // Preserve color profile.
@@ -92,37 +101,45 @@ function saveNewVersion( doc, docName ) {
   psd_Opt.spotColors        = true; // Preserve spot colors.
 	
   // Save active document in the Auto Save folder
-  doc.saveAs( File( outputPath + '/' + docName + ' ' + shortDescription + suffix + psd ), psd_Opt, true );
+	var divider = (useDocName || descPrompt) ? ' - ' : '';
+	var filename = 'v' + number + divider + (useDocName ? docName : '') + shortDescription + psd;
+  doc.saveAs( File( outputPath + '/' + filename ), psd_Opt, true );
 
   app.beep();
 	
-	if ( notify ) {
-		alert( 'Version saved: \n '+ docName + ' ' + shortDescription + suffix + psd );
+	var verisonsList = outputFolder.getFiles( '*' + psd );
+	var versionsLength2 = verisonsList.length;
+	if ( notify && versionsLength1 < versionsLength2 ) {
+		alert( 'File version saved: \n '+ filename );
+	}
+	else if ( notify && versionsLength1 >= versionsLength2 ) {
+		alert( 'Oopsies... File was not saved correctly' );
 	}
 
 }
 
-function getVersionNumber( verisonsList ) {
+function getVersionNumber( docName, verisonsList ) {
 	
-	var os = $.os;
-	
-	var suffix = 0;
+	var number = 0;
 	for ( var i = 0; i < verisonsList.length; i++ ) {
 		
 		var file = verisonsList[i];
 		var fileName = File(file).displayName;
-		var fn = fileName.substring(0, fileName.lastIndexOf('.'));
-		var versionNumber = fn.match(/[0-9]+$/);
+		fileName = fileName.substring(0, fileName.lastIndexOf('.'));
+		
+		// Start looking for the version number if PSD filename contains doc name...
+		var versionNumber = fileName.match(/^v[0-9]*/);
 		
 		if ( versionNumber ) {
-			versionNumber = parseInt( versionNumber, 10);
-			if ( suffix < versionNumber ) {
-				suffix = versionNumber;
+			versionNumber = versionNumber[0].replace('v','');
+			versionNumber = parseInt( versionNumber ,10);
+			if ( number < versionNumber ) {
+				number = versionNumber;
 			}
 		}
 		
 	}
 	
-	return suffix + 1;
+	return number + 1;
 	
 }
